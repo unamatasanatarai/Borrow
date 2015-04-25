bbc3986be89295232004fe109303d80d12e937c5
unamatasanatarai <unamatasanatarai@gmail.com>
2015-04-25 15:55:58 +0200

    automagic

--- app/bootstrap/start.php
+++ app/bootstrap/start.php
@@ -1,7 +1,5 @@
 <?php
-ob_start();
 include ROOT . 'app/bootstrap/bootstrap.php';
 
-$router = new Borrow\Dispatcher();
-$router->dispatch();
-ob_end_flush();
+$app = new Borrow\App();
+$app->start();
